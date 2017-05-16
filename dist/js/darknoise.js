eb932d46a6e7bb4bb86a1022d0c6d586b4cb81a8
unknown <pete.warrington@gmail.com>
2017-05-15 23:17:13 -0400

    fixing autoplay and minor css tweaks

--- dist/js/darknoise.js
+++ dist/js/darknoise.js
@@ -1,12 +1,26 @@
 $( document ).ready(function() {
-    $( "audio" ).click(function( event ) {
-        alert( "Thanks for visiting!" );
-    });
-	function play(){
-		$( "audio1" ).play();
-	}
-	function stop(){
-		$( "audio1" ).stop();
-	}
+    var stream = {
+		title: "Truck",
+		wav: "../../assets/sounds/truck-backup.wav"
+	},
+	ready = false;
+
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function (event) {
+			ready = true;
+			$(this).jPlayer("setMedia", stream);
+			$(this).jPlayer("setMedia", stream).jPlayer("play");
+		},
+		loop: true,
+		canplay: function() {
+		   $("#jquery_jplayer_1").jPlayer("play");
+		},  
+		swfpath:"js",
+		volume: "0.2",
+		supplied: "wav",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false
+	});
  
 });
